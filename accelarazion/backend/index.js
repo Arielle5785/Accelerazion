8a54b7e95bed76b56636e89e9715ec3ba609d0f9
Arielle5785 <arielle.benadi@gmail.com>
2025-02-11 16:25:28 +0200

    updates on index.js for render to get the path for prod, and updates on environments in render itself

--- accelarazion/backend/index.js
+++ accelarazion/backend/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
-// const router = express.Router();
-const { sendEmail } = require("./features/emailService.js");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const allRouter = require("./routes/allRouter.js");
 const path = require('path')
 require("dotenv").config();
+
 
 // const { db } = require("./config/db.js");
 
@@ -15,20 +14,24 @@ app.listen(PORT || 5000, () => {
   console.log(`run on ${PORT || 5000}`);
 });
 
-// app.use(express.json());
-// const bodyParser = require('body-parser');
 app.use(express.json()); // Correct spelling and usage
 
 app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:5174','http://localhost:5173','https://accelerazion.onrender.com']
+    // origin: ['http://localhost:5174','http://localhost:5173','https://accelerazion.onrender.com'|| "*"]
+    origin: ['http://localhost:5174'|| "*"]
   })
 );
 
 app.use("/api", allRouter);
 
+app.use(express.static(path.join(__dirname, "../dist")));
+app.get("*", (req, res) => {
+  console.log(req);
+  res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
+});
 
 
 
